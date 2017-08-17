ab8eac720a243e9d25caf950bd8e5275305bf4ea
JinendraMehta <30315853+JinendraMehta@users.noreply.github.com>
2017-08-18 00:44:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -99,6 +99,10 @@ app.get('/article-three',function(req,res){
     res.send('Article three is requested and will be served soon.');
 });
 
+app.get('/profile',function(req,res){
+    res.sendFile(path.join(__dirname, 'profile', 'index.html'));
+});
+
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
